e8b20bd4df00fabaa89d23700cd398b7db57b504
Kingask-dark <kingask1234@gmail.com>
2024-01-02 18:05:01 +0545

    modification

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,26 +7,48 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public CharacterController controller;
     public Transform cam;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
     public float speed = 0.1f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-
+    bool isGrounded;
     // Update is called once per frame
+
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
- 
-        //right is the red Axis, foward is the blue axis
-        // Vector3 move = transform.right * x + transform.forward * z;
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         Vector3 direction = new Vector3(x,0f,z).normalized;
         if(direction.magnitude >= 0.1f){
-            
-        float targetAngle = Mathf.Atan2(direction.x,direction.z)*Mathf.Rad2Deg + cam.eulerAngles.y;
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothVelocity,turnSmoothTime);
-        transform.rotation = Quaternion.Euler(0f,angle,0f);
-        Vector3 movDirection = Quaternion.Euler(0f,targetAngle,0f)* Vector3.forward;
-        controller.Move(speed * Time.deltaTime * movDirection.normalized);
-    }
+            float targetAngle = Mathf.Atan2(direction.x,direction.z)*Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothVelocity,turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f,angle,0f);
+            Vector3 movDirection = Quaternion.Euler(0f,targetAngle,0f)* Vector3.forward;
+            controller.Move(speed * Time.deltaTime * movDirection.normalized);
         }
+
+        if(controller.isGrounded){
+            float groundedGravity = -.05f;
+            direction.y = groundedGravity;            
+        }
+        else{
+            direction.y += gravity;
+        }
+
+        //  if(isGrounded && direction.y < 0){
+        //     direction.y = -2f;
+        // }
+        //check if the player is on the ground so he can jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //the equation for jumping
+           direction.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        controller.Move(direction * Time.deltaTime);
+    }
 }
--- Assets/Scripts/animationStateController.cs
+++ Assets/Scripts/animationStateController.cs
@@ -6,28 +6,40 @@ public class animationStateController : MonoBehaviour
 {
     Animator animator;
     int isWalkingHash;
-
+    int isJumpingHash;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
+        isJumpingHash = Animator.StringToHash("isJumping");
     }
 
     // Update is called once per frame
     void Update()
     {
         bool isWalking = animator.GetBool(isWalkingHash);
+        bool isJumping = animator.GetBool(isJumpingHash);
+
+        bool spacePressed = Input.GetKey(KeyCode.Space);
         bool forwardPressed = Input.GetKey(KeyCode.W);
         bool backwardPressed = Input.GetKey(KeyCode.S);
         bool leftPressed = Input.GetKey(KeyCode.A);
         bool rightPressed = Input.GetKey(KeyCode.D);
+
         if (!isWalking &&( forwardPressed || backwardPressed || leftPressed || rightPressed)){
             animator.SetBool(isWalkingHash, true);
         }
         
-        if (isWalking && !( forwardPressed || backwardPressed || leftPressed || rightPressed)){
+        if (isWalking && ( !forwardPressed || !backwardPressed || !leftPressed || !rightPressed)){
             animator.SetBool(isWalkingHash, false);
+        }
+
+        if(!isJumping && spacePressed){
+            animator.SetBool(isJumpingHash,true);
+        }
+        if(isJumping && !spacePressed){
+            animator.SetBool(isJumpingHash,false);
         }
     }
 }
